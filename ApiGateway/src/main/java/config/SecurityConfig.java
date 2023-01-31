13365eb55ba4a4aeaf343d6ad4881c06dab67cc1
Afroze Amjad <afroze.amjad@confiz.com>
2023-01-31 11:45:53 +0500

    Fixed configs

--- ApiGateway/src/main/java/config/SecurityConfig.java
+++ ApiGateway/src/main/java/config/SecurityConfig.java
@@ -1,19 +1,19 @@
-package config;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-@EnableWebFluxSecurity
-public class SecurityConfig {
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
-                .oauth2Login(Customizer.withDefaults());
-        http.csrf().disable();
-        return http.build();
-    }
-}
+//package config;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//@EnableWebFluxSecurity
+//public class SecurityConfig {
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+//        http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+//                .oauth2Login(Customizer.withDefaults());
+//        http.csrf().disable();
+//        return http.build();
+//    }
+//}
