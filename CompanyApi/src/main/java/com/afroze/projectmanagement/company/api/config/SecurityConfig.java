9fbec469ccc271817774af8bd70f84c7c23e64da
Afroze Amjad <afroze.amjad@confiz.com>
2023-01-31 16:54:00 +0500

    Auth somewhat working according to https://github.com/spring-cloud-samples/sample-gateway-oauth2login

---
+++ CompanyApi/src/main/java/com/afroze/projectmanagement/company/api/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.afroze.projectmanagement.company.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        http
+                .authorizeExchange()
+                .pathMatchers("/company/**").hasAnyAuthority()//hasAuthority("read:company")
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+}
