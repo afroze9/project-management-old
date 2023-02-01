139ece55e4e68875603c701aaa3686eb1174f70d
Afroze Amjad <afroze.amjad@confiz.com>
2023-02-02 01:40:37 +0500

    Adding stuff to Project Api

---
+++ ProjectApi/src/main/java/com/afroze/projectmanagement/project/api/ui/model/HttpResponseModel.java
@@ -0,0 +1,45 @@
+package com.afroze.projectmanagement.project.api.ui.model;
+
+public class HttpResponseModel<T> {
+    private T data;
+    private boolean isError;
+    private String errorMessage;
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public boolean isError() {
+        return isError;
+    }
+
+    public void setError(boolean error) {
+        isError = error;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    protected HttpResponseModel(T data, boolean isError, String errorMessage) {
+        this.data = data;
+        this.isError = isError;
+        this.errorMessage = errorMessage;
+    }
+
+    public static <T> HttpResponseModel<T> Success(T data) {
+        return new HttpResponseModel<>(data, false, "");
+    }
+
+    public static <T> HttpResponseModel<T> Failure(T data, String errorMessage) {
+        return new HttpResponseModel<>(data, true, errorMessage);
+    }
+}
